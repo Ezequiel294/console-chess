ace29a6b5d088326b4f03d43e9c155223a088715
Ezequiel Buck <ezequiel.bm04@gmail.com>
2024-11-17 18:39:17 -0600

    simple structs ass starting point

--- main.c
+++ main.c
@@ -2,9 +2,19 @@
 Ezequiel Buck
 Programming Principles
 Final Project
+Console Chess Game
 */
 
 #include <stdio.h>
+
+typedef struct {
+} Board_t;
+
+typedef struct {
+    char name[1];
+    char color[10];
+    char id[10];
+} Piece_t;
 
 int main(void) {
 
