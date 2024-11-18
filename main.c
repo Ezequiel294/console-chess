803091fad5656d695ca806a15be247360e875532
Ezequiel Buck <ezequiel.bm04@gmail.com>
2024-11-18 09:37:02 -0600

    added board for blacks perspective

--- main.c
+++ main.c
@@ -12,11 +12,10 @@ Console Chess Game
 typedef struct {
   wchar_t icon;
   char color;
-  char id[10];
   char position[2];
 } Piece_t;
 
-void init_board(Piece_t[8][8]);
+void init_board(Piece_t board[8][8]);
 void print_board(Piece_t board[8][8]);
 
 int main(void) {
@@ -32,40 +31,51 @@ int main(void) {
   init_board(board);
   print_board(board);
 
+  // TODO: Make 2 linked lists to store the player's captures
+  // TODO: Make a linked list to store the moves made
+
   return 0;
 }
 
 void print_board(Piece_t board[8][8]) {
-  wprintf(L"\n   a   b   c   d   e   f   g   h\n");
-  wprintf(L" +---+---+---+---+---+---+---+---+\n");
+  wprintf(L"\n   a   b   c   d   e   f   g   h               h   g   f   e   d "
+          L"  c   b   a\n");
+  wprintf(L" +---+---+---+---+---+---+---+---+           "
+          L"+---+---+---+---+---+---+---+---+\n");
   for (int i = 0; i < 8; i++) {
     wprintf(L"%d|", 8 - i);
     for (int j = 0; j < 8; j++) {
       wprintf(L" %lc |", board[i][j].icon);
     }
-    wprintf(L" %d\n", 8 - i);
-    wprintf(L" +---+---+---+---+---+---+---+---+\n");
+    wprintf(L" %d        %d|", 8 - i, i + 1);
+    for (int j = 7; j >= 0; j--) {
+      wprintf(L" %lc |", board[7 - i][j].icon);
+    }
+    wprintf(L" %d\n", i + 1);
+    wprintf(L" +---+---+---+---+---+---+---+---+           "
+            L"+---+---+---+---+---+---+---+---+\n");
   }
-  wprintf(L"   a   b   c   d   e   f   g   h\n");
+  wprintf(L"   a   b   c   d   e   f   g   h               h   g   f   e   d   "
+          L"c   b   a\n");
 }
 
 void init_board(Piece_t board[8][8]) {
-  Piece_t temp_board[8][8] = {{{L'♖', 'w', "Ra1"},
-                               {L'♘', 'w', "Nb1"},
-                               {L'♗', 'w', "Bc1"},
-                               {L'♕', 'w', "Qd1"},
-                               {L'♔', 'w', "Ke1"},
-                               {L'♗', 'w', "Bf1"},
-                               {L'♘', 'w', "Ng1"},
-                               {L'♖', 'w', "Rh1"}},
-                              {{L'♙', 'w', "Pa2"},
-                               {L'♙', 'w', "Pb2"},
-                               {L'♙', 'w', "Pc2"},
-                               {L'♙', 'w', "Pd2"},
-                               {L'♙', 'w', "Pe2"},
-                               {L'♙', 'w', "Pf2"},
-                               {L'♙', 'w', "Pg2"},
-                               {L'♙', 'w', "Ph2"}},
+  Piece_t temp_board[8][8] = {{{L'♖', 'w', "a1"},
+                               {L'♘', 'w', "b1"},
+                               {L'♗', 'w', "c1"},
+                               {L'♕', 'w', "d1"},
+                               {L'♔', 'w', "e1"},
+                               {L'♗', 'w', "f1"},
+                               {L'♘', 'w', "g1"},
+                               {L'♖', 'w', "h1"}},
+                              {{L'♙', 'w', "a2"},
+                               {L'♙', 'w', "b2"},
+                               {L'♙', 'w', "c2"},
+                               {L'♙', 'w', "d2"},
+                               {L'♙', 'w', "e2"},
+                               {L'♙', 'w', "f2"},
+                               {L'♙', 'w', "g2"},
+                               {L'♙', 'w', "h2"}},
                               {{L' ', ' ', "a3"},
                                {L' ', ' ', "b3"},
                                {L' ', ' ', "c3"},
@@ -98,21 +108,21 @@ void init_board(Piece_t board[8][8]) {
                                {L' ', ' ', "f6"},
                                {L' ', ' ', "g6"},
                                {L' ', ' ', "h6"}},
-                              {{L'♟', 'b', "Pa7"},
-                               {L'♟', 'b', "Pb7"},
-                               {L'♟', 'b', "Pc7"},
-                               {L'♟', 'b', "Pd7"},
-                               {L'♟', 'b', "Pe7"},
-                               {L'♟', 'b', "Pf7"},
-                               {L'♟', 'b', "Pg7"},
-                               {L'♟', 'b', "Ph7"}},
-                              {{L'♜', 'b', "Ra8"},
-                               {L'♞', 'b', "Nb8"},
-                               {L'♝', 'b', "Bc8"},
-                               {L'♛', 'b', "Qd8"},
-                               {L'♚', 'b', "Ke8"},
-                               {L'♝', 'b', "Bf8"},
-                               {L'♞', 'b', "Ng8"},
-                               {L'♜', 'b', "Rh8"}}};
+                              {{L'♟', 'b', "a7"},
+                               {L'♟', 'b', "b7"},
+                               {L'♟', 'b', "c7"},
+                               {L'♟', 'b', "d7"},
+                               {L'♟', 'b', "e7"},
+                               {L'♟', 'b', "f7"},
+                               {L'♟', 'b', "g7"},
+                               {L'♟', 'b', "h7"}},
+                              {{L'♜', 'b', "a8"},
+                               {L'♞', 'b', "b8"},
+                               {L'♝', 'b', "c8"},
+                               {L'♛', 'b', "d8"},
+                               {L'♚', 'b', "e8"},
+                               {L'♝', 'b', "f8"},
+                               {L'♞', 'b', "g8"},
+                               {L'♜', 'b', "h8"}}};
   memcpy(board, temp_board, sizeof(temp_board));
 }
